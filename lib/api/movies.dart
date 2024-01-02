98b16f6cb99e53a3057a2bd4091a8d7f2c1ab132
Lukman350 <nulhakiml725@gmail.com>
2024-01-02 08:05:56 +0700

    Update API endpoints and add pagination support

--- lib/api/movies.dart
+++ lib/api/movies.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:http/http.dart' as http;
 import 'package:movieflix/models/movie_details_model.dart';
 import 'dart:convert' as convert;
@@ -7,9 +8,10 @@ import 'package:movieflix/models/movieless_model.dart';
 
 class Api {
   static const String apiUrl = 'https://movieflix-api-seven.vercel.app/api';
+  // static const String apiUrl = 'http://localhost:3000/api';
 
-  static Future<List<Movie>> getPopularMovies() async {
-    final response = await http.get(Uri.parse('$apiUrl/get_popular'));
+  static Future<List<Movie>> getPopularMovies({int page = 1}) async {
+    final response = await http.get(Uri.parse('$apiUrl/popular/?page=$page'));
 
     if (response.statusCode == 200) {
       final jsonResponse = convert.jsonDecode(response.body);
@@ -25,25 +27,31 @@ class Api {
     }
   }
 
-  static Future<List<MovieLess>> getNowPlayingMovies() async {
-    final response = await http.get(Uri.parse('$apiUrl/get_now_playing'));
+  static Future getNowPlayingMovies({int page = 1, bool full = false}) async {
+    final response = await http.get(
+        Uri.parse('$apiUrl/now_playing?page=$page${full ? '&full=true' : ''}'));
 
     if (response.statusCode == 200) {
       final jsonResponse = convert.jsonDecode(response.body);
       final List<MovieLess> movies = [];
+      final List<Movie> fullMovies = [];
 
       for (var movie in jsonResponse) {
-        movies.add(MovieLess.fromJson(movie));
+        if (!full) {
+          movies.add(MovieLess.fromJson(movie));
+        } else {
+          fullMovies.add(Movie.fromJson(movie));
+        }
       }
 
-      return movies;
+      return full == true ? fullMovies : movies;
     } else {
       throw Exception('Failed to load movies');
     }
   }
 
-  static Future getMovieDetails(int movieId, {bool? lessData = false}) async {
-    final response = await http.get(Uri.parse('$apiUrl/get_movie/$movieId'));
+  static Future getMovieDetails(int movieId, {bool lessData = false}) async {
+    final response = await http.get(Uri.parse('$apiUrl/movie/$movieId'));
 
     if (response.statusCode == 200) {
       final jsonResponse = convert.jsonDecode(response.body);
@@ -56,13 +64,19 @@ class Api {
     }
   }
 
-  static Future<Movie> searchMovies(String query) async {
-    final response = await http.get(Uri.parse('$apiUrl/search_movie/$query'));
+  static Future<List<Movie>> searchMovies(String query, {int page = 1}) async {
+    final response =
+        await http.get(Uri.parse('$apiUrl/search/?query=$query&page=$page'));
 
     if (response.statusCode == 200) {
       final jsonResponse = convert.jsonDecode(response.body);
+      final List<Movie> movies = [];
 
-      return Movie.fromJson(jsonResponse);
+      for (var movie in jsonResponse) {
+        movies.add(Movie.fromJson(movie));
+      }
+
+      return movies;
     } else {
       throw Exception('Failed to load movie');
     }
