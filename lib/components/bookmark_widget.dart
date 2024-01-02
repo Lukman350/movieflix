ed8c686b3b1d8495e1672a276ea24ddfc5d9babb
Lukman350 <nulhakiml725@gmail.com>
2024-01-02 08:06:05 +0700

    Refactor BookmarkButton to use StatefulWidget

--- lib/components/bookmark_widget.dart
+++ lib/components/bookmark_widget.dart
@@ -1,12 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class BookmarkButton extends StatelessWidget {
-  final Future<SharedPreferences> _preferences =
-      SharedPreferences.getInstance();
+class BookmarkButton extends StatefulWidget {
   final int movieId;
 
-  BookmarkButton({super.key, required this.movieId});
+  const BookmarkButton({super.key, required this.movieId});
+
+  @override
+  State<BookmarkButton> createState() => _BookmarkButtonState();
+}
+
+class _BookmarkButtonState extends State<BookmarkButton> {
+  final Future<SharedPreferences> _preferences =
+      SharedPreferences.getInstance();
+  SharedPreferences? prefs;
+  bool movieBookmarked = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _preferences.then((value) => {
+          setState(() {
+            prefs = value;
+          })
+        });
+  }
 
   Future<void> _saveBookmark(int movieId) async {
     final SharedPreferences prefs = await _preferences;
@@ -38,37 +56,52 @@ class BookmarkButton extends StatelessWidget {
     return bookmarks.contains(movieId.toString());
   }
 
-  bool _checkBookmark(int movieId) {
-    bool isMovieBookmarked = false;
-    _isBookmarked(movieId).then((value) => isMovieBookmarked = value);
-    return isMovieBookmarked;
+  bool _isMovieBookmarked(int movieId) {
+    final SharedPreferences? preferences = prefs;
+
+    if (preferences == null) {
+      return false;
+    }
+
+    final List<String> bookmarks =
+        preferences.getStringList('bookmarks') ?? <String>[];
+
+    return bookmarks.contains(movieId.toString());
   }
 
   @override
   Widget build(BuildContext context) {
     return IconButton(
-      onPressed: () async {
-        final bool isBookmarked = await _isBookmarked(movieId);
-        final snackBar = SnackBar(
-          content: Text(isBookmarked ? 'Bookmark removed!' : 'Bookmark added!'),
-          duration: const Duration(seconds: 2),
-        );
+        onPressed: () async {
+          final bool isBookmarked = await _isBookmarked(widget.movieId);
+          final snackBar = SnackBar(
+            content:
+                Text(isBookmarked ? 'Bookmark removed!' : 'Bookmark added!'),
+            duration: const Duration(milliseconds: 500),
+          );
 
-        if (isBookmarked) {
-          _removeBookmark(movieId);
-        } else {
-          _saveBookmark(movieId);
-        }
+          if (isBookmarked) {
+            _removeBookmark(widget.movieId);
+            setState(() {
+              movieBookmarked = false;
+            });
+          } else {
+            _saveBookmark(widget.movieId);
+            setState(() {
+              movieBookmarked = true;
+            });
+          }
 
-        if (context.mounted) {
-          ScaffoldMessenger.of(context).showSnackBar(snackBar);
-        }
-      },
-      icon: Icon(
-        _checkBookmark(movieId) ? Icons.bookmark : Icons.bookmark_add_outlined,
-        size: 26,
-        color: Colors.black,
-      ),
-    );
+          if (context.mounted) {
+            ScaffoldMessenger.of(context).showSnackBar(snackBar);
+          }
+        },
+        icon: Icon(
+          _isMovieBookmarked(widget.movieId) || movieBookmarked
+              ? Icons.bookmark
+              : Icons.bookmark_add_outlined,
+          size: 26,
+          color: Colors.black,
+        ));
   }
 }
