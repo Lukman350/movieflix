d2042d5c3c0aa6a00e1cf25c2b552e289798a840
Lukman350 <nulhakiml725@gmail.com>
2024-01-02 08:07:10 +0700

    Add movie search functionality and pagination

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
+import 'package:movieflix/api/movies.dart';
+import 'package:movieflix/components/movie_card.dart';
+import 'package:movieflix/components/skeletons/movie_card.dart';
+import 'package:movieflix/models/movie_model.dart';
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
@@ -9,7 +13,91 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
-  String _searchQuery = '';
+  String _searchText = '';
+  static const _pageSize = 20;
+  final PagingController<int, Movie> _pagingController =
+      PagingController(firstPageKey: 1);
+
+  @override
+  void initState() {
+    super.initState();
+    _pagingController.addPageRequestListener((pageKey) {
+      _fetchPage(pageKey);
+    });
+  }
+
+  Future<void> _fetchPage(int pageKey) async {
+    try {
+      final newItems = await Api.searchMovies(_searchText, page: pageKey);
+      final isLastPage = newItems.length < _pageSize;
+      if (isLastPage) {
+        _pagingController.appendLastPage(newItems);
+      } else {
+        final nextPageKey = pageKey + newItems.length;
+        _pagingController.appendPage(newItems, nextPageKey);
+      }
+    } catch (error) {
+      _pagingController.error = error;
+    }
+  }
+
+  Widget _buildBody() {
+    return PagedListView<int, Movie>(
+      padding: const EdgeInsets.all(6.0),
+      pagingController: _pagingController,
+      scrollDirection: Axis.vertical,
+      builderDelegate: PagedChildBuilderDelegate<Movie>(
+        animateTransitions: true,
+        firstPageErrorIndicatorBuilder: (_) => const Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.search,
+                color: Color.fromARGB(255, 17, 14, 71),
+                size: 80,
+              ),
+              SizedBox(height: 16),
+              Text(
+                'Search for movies',
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w400,
+                  color: Color.fromARGB(255, 17, 14, 71),
+                ),
+              ),
+            ],
+          ),
+        ),
+        noItemsFoundIndicatorBuilder: (_) => const Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.error,
+                color: Colors.red,
+                size: 80,
+              ),
+              SizedBox(height: 16),
+              Text(
+                'No movies found',
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.red,
+                ),
+              ),
+            ],
+          ),
+        ),
+        newPageProgressIndicatorBuilder: (_) => const MovieCardSkeletonSingle(),
+        itemBuilder: (context, item, index) => MovieCard(
+          grid: true,
+          movie: item,
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,25 +107,29 @@ class _SearchScreenState extends State<SearchScreen> {
         title: TextField(
           autofocus: true,
           decoration: const InputDecoration(
-            hintText: 'Search movies...',
-            suffixIcon: Icon(Icons.search),
-          ),
+              hintText: 'Search movies...',
+              hintStyle: TextStyle(
+                color: Colors.grey,
+              ),
+              suffixIcon: Icon(Icons.search)),
           keyboardType: TextInputType.text,
           textInputAction: TextInputAction.search,
           onSubmitted: (value) {
             setState(() {
-              _searchQuery = value;
+              _searchText = value;
             });
+
+            _pagingController.refresh();
           },
         ),
       ),
-      body: _searchQuery.isEmpty
-          ? const Center(
-              child: Text('Search for movies!'),
-            )
-          : Center(
-              child: Text('Search results for $_searchQuery'),
-            ),
+      body: Padding(padding: const EdgeInsets.all(8.0), child: _buildBody()),
     );
+  }
+
+  @override
+  void dispose() {
+    _pagingController.dispose();
+    super.dispose();
   }
 }
